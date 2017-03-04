154def2e855db2813304231e17dafd793300d29f
Matthew Harwood <matthhar12@gmail.com>
2017-03-04 09:36:45 -0800

    - removed angular added fastdom.

--- src/greeter.ts
+++ src/greeter.ts
@@ -1,9 +1,10 @@
-const module =  angular.module('tealeaves', []);
-console.log(module);
+import fastDom from "FastDom";
+
+console.log(fastDom);
 export class Greeter {
   constructor(public greeting: string) {
   }
   public greet(): string {
     return "<h1>" + this.greeting + "</h1>";
   }
-};
+}
