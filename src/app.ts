cd85dbdb264cde890a9d33044cffcb00d80fc7d9
matthew harwood <matthhar12@gmail.com>
2017-03-08 20:10:49 -0800

    - Added angular.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import { Directive } from './scaffolding/directive';
 import {appState} from './scaffolding/appState';
-
+import * as fastdom from 'FastDom';
 import {remove} from './scaffolding/directive.actions';
+import * as angular from "angular";
 
+const module = angular.module('app', []);
+console.log(module);
 @Directive({selector: 'app'})
 class App{}
 
@@ -15,6 +18,7 @@ class El2{}
 @Directive({selector: 'El3'})
 class El3{}
 
+console.log(fastdom);
 const x = new App();
 const x1 = new El1();
 const x2 = new El2();
