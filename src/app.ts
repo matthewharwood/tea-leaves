4de91be348f8b3d7fcbc5054568e8223dc82294f
matthew harwood <matthhar12@gmail.com>
2017-03-10 00:31:00 -0800

    - Added the landing styles.

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,4 @@
-import { Directive } from './scaffolding/directive';
-import {appState} from './scaffolding/appState';
-import * as fastdom from 'FastDom';
-import {remove} from './scaffolding/directive.actions';
 import * as angular from "angular";
 
 const module = angular.module('app', []);
-console.log(module);
-@Directive({selector: 'app'})
-class App{}
-
-@Directive({selector: 'El1'})
-class El1{}
-
-@Directive({selector: 'El2'})
-class El2{}
-
-@Directive({selector: 'El3'})
-class El3{}
-
-console.log(fastdom);
-const x = new App();
-const x1 = new El1();
-const x2 = new El2();
-const x3 = new El3();
-appState.dispatch(remove('app'));
-console.log(appState.getState());
 
--- src/scaffolding/action.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by matty on 3/7/17.
- */
-
-
-export interface Action {
-  type: any,
-  payload: any
-}
