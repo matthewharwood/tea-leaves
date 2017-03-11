811b1966499288ae0cff390207be0f4ab047f677
matthew harwood <matthhar12@gmail.com>
2017-03-11 08:13:09 -0800

    Added browser sync and better logging MUST RUN NPM INSTALL AGAIN.

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,50 @@ const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
-
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+const util = require("gulp-util");
+const changed = require("gulp-changed");
 const templatePath = './src/public/**/*.pug';
 const cssPath = 'src/public/assets/styles/**/*.scss';
 
 gulp.task('html', function(){
+  let nSrc=0, nDes=0;
   return gulp.src(templatePath)
       .pipe(pug())
       .pipe(gulp.dest('src/public'))
+      .on("data", function() { nDes+=1;})
+      .on("finish", function() {
+          util.log("Results: src/public/**/*.pug");
+          util.log("# src files: ", nSrc);
+          util.log("# dest files:", nDes);
+      });
 
 });
 
 gulp.task('images', () => {
+  let nSrc=0, nDes=0;
   return gulp.src('dist/public/assets/images/**/*')
       .pipe(imagemin({
         progressive: true,
         svgoPlugins: [{removeViewBox: false}],
         use: [pngquant()]
       }))
-      .pipe(gulp.dest('dist/public/assets/images'));
+      .pipe(gulp.dest('dist/public/assets/images'))
+      .on("data", function() { nDes+=1;})
+      .on("finish", function() {
+          util.log("Results: dist/public/assets/images/**/*");
+          util.log("# src files: ", nSrc);
+          util.log("# dest files:", nDes);
+      })
+      .pipe(browserSync.stream());
 });
 
 
 gulp.task('css', function () {
   const postcss    = require('gulp-postcss');
   const sourcemaps = require('gulp-sourcemaps');
+  let nSrc=0, nDes=0;
   const processors = [
       require('lost'),
       require('rucksack-css'),
@@ -39,12 +58,28 @@ gulp.task('css', function () {
       .pipe( sourcemaps.init() )
       .pipe( postcss(processors) )
       .pipe( sourcemaps.write('.') )
-      .pipe( gulp.dest('src/public/assets/styles') )
+      .pipe( gulp.dest('src/public/assets/styles'))
+      .on("data", function() { nDes+=1;})
+      .on("finish", function() {
+          util.log("Results for app/**/*.js");
+          util.log("# src files: ", nSrc);
+          util.log("# dest files:", nDes);
+      })
+      .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['html', 'css']);
 
-gulp.task('watch', ()=> {
-    gulp.watch(cssPath, ['css']);
-    gulp.watch(templatePath, ['html']);
+gulp.task('browser-sync', ()=> {
+    browserSync({
+        proxy: "localhost:8080",  // local node app address
+        port: 6969,  // use *different* port than above
+        notify: true
+    });
 });
+
+gulp.task('watch', ['browser-sync'], ()=> {
+    gulp.watch(cssPath, ['css']).on('change', browserSync.reload);
+    gulp.watch(templatePath, ['html']).on('change', browserSync.reload);
+});
+
