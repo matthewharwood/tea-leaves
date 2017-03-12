26cd38ae8c1df3f00fe15abed817617c230c9acf
Angus MacDonald <angusamacdonald@hotmail.com>
2017-03-11 16:19:17 -0800

    Added stop-down and stop-up options to waypoint.
    Also trying to untrack these pug html files.

--- src/ts/parallax/directive.ts
+++ src/ts/parallax/directive.ts
@@ -3,6 +3,8 @@ import * as fastdom from 'FastDom';
 
 interface IParallaxDirectiveAttributes extends ng.IAttributes {
     speed: string;
+    down: string;
+    up: string;
 }
 
 class ParallaxDirective implements ng.IDirective {
@@ -18,24 +20,35 @@ class ParallaxDirective implements ng.IDirective {
         attr: IParallaxDirectiveAttributes,
     ): void {
         const speed: number = 1 - parseFloat(attr.speed);
-        update(element[0] as HTMLElement, speed);
+        const isDown: boolean = !('stopDown' in attr);
+        const isUp: boolean = !('stopUp' in attr);
+        update(element[0] as HTMLElement, speed, isDown, isUp);
     }
 }
 
 export const parallax = angular.module('parallax', []);
 parallax.directive('parallax', ParallaxDirective.instance);
 
-function update(element: HTMLElement, speed: number) {
+function update(element: HTMLElement, speed: number, down: boolean = true, up: boolean = true) {
     fastdom.measure(() => {
         const distanceFromCenter = getDistanceFromWindowCenter(element);
+        let translateY;
+        if (
+            (distanceFromCenter < 0 && !down) ||
+            (distanceFromCenter > 0 && !up)
+        ) {
+            translateY = 0;
+        } else {
+            translateY = distanceFromCenter * speed;
+        }
+
         fastdom.mutate(() => {
-            element.style.transform =
-                `translateY(${distanceFromCenter * speed}px)`;
+            element.style.transform = `translateY(${translateY}px)`;
         });
     });
 
     window.requestAnimationFrame(() => {
-        update(element, speed);
+        update(element, speed, down, up);
     });
 }
 
