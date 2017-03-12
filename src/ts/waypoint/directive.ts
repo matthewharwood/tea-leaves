3fc62af03647b7c5b3c1ddcb7ee9a6fe714173f5
Angus MacDonald <angusamacdonald@hotmail.com>
2017-03-11 21:21:46 -0800

    Added mobile fade for hero

--- src/ts/waypoint/directive.ts
+++ src/ts/waypoint/directive.ts
@@ -2,7 +2,7 @@ import * as angular from "angular";
 import * as fastdom from 'FastDom';
 
 const ACTIVE_CLASS = 'waypoint--active';
-const THRESHOLD = 0.1;
+const DEFAULT_THRESHOLD = 0.1;
 
 class WaypointDirective implements ng.IDirective {
     public static instance(): ng.IDirective {
@@ -16,24 +16,27 @@ class WaypointDirective implements ng.IDirective {
         element: ng.IAugmentedJQuery,
         attr: ng.IAttributes,
     ): void {
-        const keepDown: boolean = 'keepDown' in attr;
-        const keepUp: boolean = 'keepUp' in attr;
-        update(element[0] as HTMLElement, keepDown, keepUp);
+        const activeAfter: boolean = 'activeAfter' in attr;
+        const activeBefore: boolean = 'activeBefore' in attr;
+        const threshold: number =
+            'threshold' in attr ?
+                parseFloat(attr.threshold) : DEFAULT_THRESHOLD;
+        update(element[0] as HTMLElement, activeAfter, activeBefore, threshold);
     }
 }
 
 export const waypoint = angular.module('waypoint', []);
 waypoint.directive('waypoint', WaypointDirective.instance);
 
-function update(element: HTMLElement, keepDown, keepUp) {
+function update(element: HTMLElement, activeAfter, activeBefore, threshold) {
     fastdom.measure(() => {
         const distanceFromCenter = getDistanceFromWindowCenter(element);
-        const thresholdDistance = window.innerHeight * THRESHOLD;
+        const thresholdDistance = window.innerHeight * threshold;
         fastdom.mutate(() => {
             if (
                 (Math.abs(distanceFromCenter) <= thresholdDistance) ||
-                (distanceFromCenter < 0 && keepDown) ||
-                (distanceFromCenter > 0 && keepUp)
+                (distanceFromCenter < 0 && activeAfter) ||
+                (distanceFromCenter > 0 && activeBefore)
             ) {
                 element.classList.add(ACTIVE_CLASS);
             } else {
@@ -43,7 +46,7 @@ function update(element: HTMLElement, keepDown, keepUp) {
     });
 
     window.requestAnimationFrame(() => {
-        update(element, keepDown, keepUp);
+        update(element, activeAfter, activeBefore, threshold);
     });
 }
 
