396996f17201140f604796966114f67d7ccb7d42
matthew harwood <matthhar12@gmail.com>
2017-03-18 13:16:41 -0700

    - Moar mobile styles.

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
 import * as angular from "angular";
-import {fade} from './ts/fade/directive';
-import {frenchOak} from './ts/french-oak/directive';
-import {modal} from './ts/modal/directive';
-import {parallax} from './ts/parallax/directive';
-import {splash} from './ts/splash/directive';
-import {stickyNav} from './ts/sticky_nav/directive';
-import {waypoint} from './ts/waypoint/directive';
-import {wipe} from './ts/wipe/directive';
+import { fade } from './ts/fade/directive';
+import { frenchOak } from './ts/french-oak/directive';
+import { modal } from './ts/modal/directive';
+import { parallax } from './ts/parallax/directive';
+import { splash } from './ts/splash/directive';
+import { stickyNav } from './ts/sticky_nav/directive';
+import { waypoint } from './ts/waypoint/directive';
+import { wipe } from './ts/wipe/directive';
 
 const module = angular.module('app', [
-    fade.name,
-    frenchOak.name,
-    modal.name,
-    parallax.name,
-    splash.name,
-    stickyNav.name,
-    waypoint.name,
-    wipe.name,
+  fade.name,
+  frenchOak.name,
+  modal.name,
+  parallax.name,
+  splash.name,
+  stickyNav.name,
+  waypoint.name,
+  wipe.name,
 ]);
