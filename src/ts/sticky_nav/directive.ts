fbf59ff0502620ba0303c919e5b778b4d9fee847
Angus MacDonald <angusamacdonald@hotmail.com>
2017-03-26 16:56:53 -0700

    Using translate3d to force HW involvement

--- src/ts/sticky_nav/directive.ts
+++ src/ts/sticky_nav/directive.ts
@@ -64,7 +64,7 @@ function update(element: HTMLElement, prevState: IStickyState = {
                 SPEED * currentState.translateDirection + currentY;
             const cappedAdjustment =
                 Math.max(-height, Math.min(0, initialAdjustment));
-            element.style.transform = `translateY(${cappedAdjustment}px)`;
+            element.style.transform = `translate3d(0, ${cappedAdjustment}px, 0)`;
         });
     });
     window.requestAnimationFrame(() => {
